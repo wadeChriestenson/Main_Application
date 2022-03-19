4fb853eb174752c2fe3dfed30a140db2e2022e48
Wade Chriestenson <wadechriestenson@gmail.com>
2022-03-19 15:01:09 -0700

    03/19/20222 - WC

--- Base_App/settings.py
+++ Base_App/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,6 @@ STATIC_ROOT = "Resume/static"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
